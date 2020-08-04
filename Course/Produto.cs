3ab953438ca6ce4ec3724d444d584364e7c851ce
Sergio Santos <swillians@gmail.com>
2020-08-04 16:52:15 -0300

    Primeira Versao

---
+++ Course/Produto.cs
@@ -0,0 +1,77 @@
+﻿using System.Globalization;
+namespace Course
+{
+    class Produto
+    {
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto()
+        {
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco) : this()
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = 0;
+        }
+
+        public string Nome
+        {
+            get { return _nome; }
+            set { 
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+        public int Quantidade
+        {
+            get { return _quantidade; }
+            set
+            {
+                _quantidade = value;
+            }
+        }
+        public double Preco
+        {
+            get { return _preco; }
+            set
+            {
+                _preco = value;
+            }
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = new DateTime(200, 8, 15, 13, 5, 58, DateTimeKind.Local);
+            DateTime d2 = new DateTime(200, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+            DateTime d3 = new DateTime(200, 8, 15, 13, 5, 58);
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Kind);
+            Console.WriteLine(d1.ToLocalTime());
+            Console.WriteLine(d1.ToUniversalTime());
+        }
+    }
+}
+
